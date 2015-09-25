585e6dec04ca1f96dabd78f2881cc418a6c90731
Renan Souza <falecomrenansouza@gmail.com>
2015-09-25 00:30:05 -0300

    Primeiros exercícios de arrays

--- arrays/exercicio_01.php
+++ arrays/exercicio_01.php
@@ -2,5 +2,23 @@
 
 // Exercícios com arrays
 
+// Push - usado para adicionar elemento ao final do array
+
+echo "<h2>Array Push</h2>";
+	$nomes = array('Renan', 'Bruno', 'Vitor');
+		array_push($nomes, 'Lucas');
+	var_dump($nomes);
+
+// Pop - usado para remover o último elemento do array
+
+echo "<h2>Array Pop</h2>";
+	array_pop($nomes);
+	var_dump($nomes);
+
+// Shift - usado para remove elemento do início do array
+
+echo "<h2>Array Shift</h2>";
+	array_shift($nomes);
+	var_dump($nomes);
 
 ?>
