fc01e7764b67f508b1e8ea042c479109328e8dea
Renan Souza <falecomrenansouza@gmail.com>
2015-09-25 00:39:40 -0300

    Comentários sobre cada uma das funções utilizadas

--- arrays/exercicio_01.php
+++ arrays/exercicio_01.php
@@ -4,26 +4,26 @@
 
 // Push - usado para adicionar elemento ao final do array
 
-echo "<h2>Array Push</h2>";
+echo "<h2>Array Push - adiciona elemento ao final do array</h2>";
 	$nomes = array('Renan', 'Bruno', 'Vitor');
 		array_push($nomes, 'Lucas');
 	var_dump($nomes);
 
 // Pop - usado para remover o último elemento do array
 
-echo "<h2>Array Pop</h2>";
+echo "<h2>Array Pop - remove último elemento do array</h2>";
 	array_pop($nomes);
 	var_dump($nomes);
 
 // Shift - usado para remove elemento do início do array
 
-echo "<h2>Array Shift</h2>";
+echo "<h2>Array Shift - remove elemento do início do array</h2>";
 	array_shift($nomes);
 	var_dump($nomes);
 
 // Unshift - usado para adicionar elemento no início do array
 
-echo "<h2>Array Unshift</h2>";
+echo "<h2>Array Unshift - adiciona elemento do início do array</h2>";
 	array_unshift($nomes, "Lucas");
 	var_dump($nomes);
 
